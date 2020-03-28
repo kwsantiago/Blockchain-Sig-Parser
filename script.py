259a0fae631dcae81eddb633cd45f035d5242987
kyle <44934418+kwsantiago@users.noreply.github.com>
2020-03-28 13:02:51 -0400

    Skip duplicate lines

--- script.py
+++ script.py
@@ -1,16 +1,21 @@
 import linecache
 import sys
 
+# -- Remove duplicates lines --
+lines_seen = set() # lines already seen
+
 count = 0
 
-while True: 
+# -- Print lines --
+while True:
     count += 1
-  
     # Get next line from file 
     line = linecache.getline('test.log', count)
     size = sys.getsizeof(line)
     # if line is empty 
     # end of file is reached 
-    if not line: 
+    if not line:
         break
-    print("Line {}: {}\nSize: {}\n".format(count, line.strip(), size))
+    if line not in lines_seen: # not a duplicate
+        print("Line {}: {}\nSize: {}\n".format(count, line.strip(), size))
+        lines_seen.add(line)
