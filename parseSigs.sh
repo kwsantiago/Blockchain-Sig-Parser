6c8829cad2da44d38ceb5e46c916afc3ed39040b
Kyle Santiago <kwsantiago@mail.usf.edu>
2020-04-11 13:19:27 -0400

    Create parseSigs.sh

---
+++ parseSigs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ $# == 1 ] # if only 1 argument
+then
+    python3 script.py $1 # execute the script with the first argument (.log file)
+    sort output.log | uniq -c | sort -rn | head -n 10 # print top 10 public keys with most sigs
+fi
+
+if [ $# == 2 ] # if 2 arguments
+then
+    python3 script.py $1 $2 # execute the script with the first arg (.log file) and second arg (pubkey)
+fi
