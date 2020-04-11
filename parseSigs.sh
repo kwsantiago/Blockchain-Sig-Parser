1c031a5c3a19bdf227a5c8f792b248278a5f25c6
Kyle Santiago <kwsantiago@mail.usf.edu>
2020-04-11 14:13:43 -0400

    Add else for error

--- parseSigs.sh
+++ parseSigs.sh
@@ -5,9 +5,9 @@ then
     python3 script.py $1 # execute the script with the first argument (.log file)
     sort output.log | uniq -c | sort -rn | head -n 10 # print top 10 public keys with most sigs
     rm output.log
-fi
-
-if [ $# == 2 ] # if 2 arguments
+elif [ $# == 2 ] # if 2 arguments
 then
     python3 script.py $1 $2 # execute the script with the first arg (.log file) and second arg (pubkey)
+else
+    echo Error: Invalid input
 fi
