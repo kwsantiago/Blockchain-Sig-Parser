471d6e20bb5623cce3ff8663a556b6099092466d
kyle <44934418+kwsantiago@users.noreply.github.com>
2020-04-11 12:18:59 -0400

    Update script.py

--- script.py
+++ script.py
@@ -18,7 +18,7 @@ def getLines_WithSig():
         if not line: # if line is empty, end of file is reached 
             break
         lineList = line.strip().split() # strip '\n' and put each string into an element of an array
-        if(lineList[2] == sys.argv[2]): # if line's pubkey and user's pubkey matches, print
+        if(lineList[2] == sys.argv[2]): # if line's pubkey and user's pubkey matches, print signatures of that pubkey
             print("Line {} Sig: {}".format(lineNum, lineList[1]))
             file.write("Line {} Sig: {}\n".format(lineNum, lineList[1]))
 
@@ -42,7 +42,7 @@ def getLines_NoSig():
         if lineList[2] not in pubkeys_seen: # if line's pubkey has not been seen
             pubkeys_seen.add(lineList[2]) # add it to pubkeys_seen
             continue
-        if lineList[2] in pubkeys_seen: # else if line's pubkey has been seen, print to file
+        if lineList[2] in pubkeys_seen: # else if line's pubkey has been seen, print pubkey to file
             file.write("{}\n".format(lineList[2]))
 
     file.close
