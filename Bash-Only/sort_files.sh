a2183b40fc8e23a423476bd10861e8c754d08727
kyle <44934418+kwsantiago@users.noreply.github.com>
2020-05-30 01:47:35 -0400

    Change name

--- Bash-Only/sort_files.sh
+++ Bash-Only/sort_files.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sort -k 3,3 filename # sort filename by public key
-awk -F\  '{print>"02/"$3}' 02.log # print sigs for each public key in files seperated by public keys.
+sort -k 3,3 test.log # sort filename by public key
+awk -F\  '{print>"test/"$3}' test.log # print sigs for each public key in files seperated by public keys.
