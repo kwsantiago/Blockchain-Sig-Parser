60e462646cb90fb5a7d1b8cdb1016774ddb092a5
kwsantiago <kwsantiago@mail.usf.edu>
2020-06-13 15:11:37 -0400

    Add comments and improve performance

--- Bash-Only/countLines.sh
+++ Bash-Only/countLines.sh
@@ -4,7 +4,8 @@ count=0
 
 for FILENAME in *; 
 do
-    numLines=$(wc -l $FILENAME | awk '{print $1}')
+    #numLines=$(wc -l $FILENAME | awk '{print $1}') # Use if naming convention is different
+    numLines="${FILENAME##*.}" # Take number of lines from the filename which is the number after the '.'
     let count+=$numLines
 done
 
