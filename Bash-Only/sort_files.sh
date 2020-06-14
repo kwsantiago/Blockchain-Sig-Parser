08bb43d94cbbcceec0f8bd5403ce9bbe8a86001a
kwsantiago <kwsantiago@mail.usf.edu>
2020-06-14 15:53:24 -0400

    Update sort for sponge

--- Bash-Only/sort_files.sh
+++ Bash-Only/sort_files.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sort -k 3,3 test.log # sort filename by public key
-awk -F\  '{print>"test/"$3}' test.log # print sigs for each public key in files seperated by public keys.
+sort -k 3,3 $1 | sponge $1 # sort filename by public key
+awk -F\  '{print>"./"$3}' $1 # print sigs for each public key in files seperated by public keys.
