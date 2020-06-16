8341f5168e1e965222f44b8e81c70ed6a454d07d
kwsantiago <kwsantiago@mail.usf.edu>
2020-06-16 16:32:54 -0400

    Comment out line

--- Bash-Only/sort_files.sh
+++ Bash-Only/sort_files.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sort -k 3,3 $1 | sponge $1 # sort filename by public key
+#sort -k 3,3 $1 | sponge $1 # sort filename by public key // May not be needed (Test accordingly)
 awk -F\  '{print>"./"$3}' $1 # print sigs for each public key in files seperated by public keys.
